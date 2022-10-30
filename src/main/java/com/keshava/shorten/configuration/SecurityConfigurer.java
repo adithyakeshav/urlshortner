0180466912c7d7ce8ad48c77d57e04d5ae9a6d46
Adithyakeshava K <adithyakak0@gmail.com>
2022-10-30 21:08:43 +0530

    Enabled CSRF

--- src/main/java/com/keshava/shorten/configuration/SecurityConfigurer.java
+++ src/main/java/com/keshava/shorten/configuration/SecurityConfigurer.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,8 +31,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().antMatchers("/api/auth").permitAll()
+        http.authorizeRequests().antMatchers("/api/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) ;
